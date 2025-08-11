149057477d172659d05b526855cf66c3ac22677f
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2025-08-11 13:20:34 +1000

    a

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+
+from data_module import (
+    display_dataset_preview,
+    display_visualisation,
+    search_data,
+)
+
+def main_menu():
+    while True:
+        print(""" _________________________________
+                 | \n=== Data Viewer Interface === |
+                 |---------------------------------| 
+                 | 1. View dataset                 |
+                 | 2. View visualisation           |
+                 | 3. Search or filter data        |
+                 | 4. Exit                         |
+                  _________________________________""")
+
+
+        choese = input(f"Select an option (1-4): ").strip()
+
+        if choese == '1':
+            display_dataset_preview()
+        elif choese == '2':
+            display_visualisation()
+        elif choese == '3':
+            search_data()
+            print("Changes saved.")
+        elif choese == '4':
+            time.sleep(1)
+            print(""" ____________________
+                     | Exiting program..  |
+                      ____________________""")
+            break
+        else:
+            print("Invalid selection. Please select a number between 1 and 4.")
+
+
+main_menu() 
